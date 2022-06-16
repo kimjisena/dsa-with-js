84bc446732cacef6ed8858ca5f5a9cf220b79730
jisena.btc <pjohmaths@gmail.com>
2022-06-16 21:39:41 +0300

    implemented binary tree

---
+++ chap6/btree.js
@@ -0,0 +1,108 @@
+/**
+ * binary trees
+ * 
+ * a binary tree has nodes that contain at most two children
+ * 
+ * important
+ * 
+ * - `size`: the total number of nodes
+ * - `height`: the maximal path from a `leaf node` to the `root node`
+ * - an empty tree has `height = -1` and `size = 0`;
+ * 
+ * formal definition
+ * 
+ * a binary tree is either:
+ * 1. the empty tree, or
+ * 2. it consists of a node and two btrees, `left` subtree and `right` subtree
+ * 
+ * binary tree constructors
+ * 1. `emptyTree()` - returns the `EmptyTree`
+ * 2. `makeTree(v, l, r)` - builds a btree from a `root node` with 
+ * label `v` and two subtrees, `l` and `r`
+ * 
+ * binary tree condition
+ * - `btree.isEmpty()` - retuns false if `btree` is non-empty
+ * 
+ * binary selectors
+ * 1. `btree.root()` - returns the value of `root node`
+ * 2. `btree.left()` - returns the `left subtree` or `EmptyTree`
+ * 3. `btree.right()` - returns the `right subtree` or `EmptyTree`
+ * 
+ * convenient derived operators
+ * - `leaf(v) = makeTree(v, EmptyTree, EmptyTree)` - returns a single node with label `v`
+ */
+
+function getBT() {
+    let label = null,
+        children;
+    
+    function isEmpty() {
+        return label === null;
+    }
+
+    if (arguments.length === 1) {
+        label = arguments[0];
+        children = {
+            left: getBT(),
+            right: getBT()
+        };
+    } else if (arguments.length === 3) {
+        label = arguments[0];
+        children = {
+            left: arguments[1],
+            right: arguments[2]
+        };
+    }
+
+    function root () {
+        if (isEmpty()) {
+            throw {
+                name: 'EmptyTreeError',
+                message: 'Can not access root value of an empty tree'
+            };
+        }
+
+        return label;
+    }
+
+    function left () {
+        if (isEmpty()) {
+            throw {
+                name: 'EmptyTreeError',
+                message: 'Can not access root value of an empty tree'
+            };
+        }
+
+        return children.left;
+    }
+
+    function right () {
+        if (isEmpty()) {
+            throw {
+                name: 'EmptyTreeError',
+                message: 'Can not access root value of an empty tree'
+            };
+        }
+
+        return children.right;
+    }
+
+
+    return {
+        isEmpty,
+        root,
+        left,
+        right
+    };
+}
+
+export function emptyTree () {
+    return getBT();
+}
+
+export function makeBT (v, l, r) {
+    if (arguments.length === 1) {
+        return getBT(arguments[0]);
+    }
+    return getBT(v, l, r);
+}
---
+++ chap6/btree.test.js
@@ -0,0 +1,50 @@
+import { emptyTree, makeBT } from "./btree";
+
+// testing binary tree functionality
+let tree1 = emptyTree();
+console.log(tree1.isEmpty()); // true
+
+let tree2 = makeBT(11);
+console.log(tree2.isEmpty()); // false
+console.log(tree2.root()); // 11
+
+let l = emptyTree();
+let r = emptyTree();
+let lr = makeBT(8, l, r);
+
+console.log(lr.isEmpty()); // false
+console.log(lr.root()); // 8
+console.log(lr.left().isEmpty()); // true
+console.log(lr.right().isEmpty()); // true
+
+// create a nested tree
+let tree3 = makeBT(
+    8,
+    makeBT(
+        3,
+        makeBT(1),
+        makeBT(
+            6,
+            emptyTree(),
+            makeBT(7))
+    ),
+    makeBT(
+        11,
+        makeBT(
+            9,
+            emptyTree(),
+            makeBT(10)
+        ),
+        makeBT(
+            14,
+            makeBT(12),
+            makeBT(15)
+        )
+    )
+);
+
+console.log(tree3.isEmpty()); // false
+console.log(tree3.root()); // 8
+console.log(tree3.left().root()); // 3
+console.log(tree3.right().root()); // 11
+console.log(tree3.left().left().left().isEmpty()); // true
