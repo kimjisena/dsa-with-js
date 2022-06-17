ae91324fe5d2eeea0f96de546c5163cb7dc1dad9
jisena.btc <pjohmaths@gmail.com>
2022-06-17 13:46:27 +0300

    implemented binary search tree + recursive insert algorithm

--- chap7/bstree.js
+++ chap7/bstree.js
@@ -1,5 +1,69 @@
+import { emptyTree, makeBT } from "../chap6/btree.js";
+
 /**
  * binary search trees
  * 
+ * a `bstree` is a variant of a binary tree which allows efficient 
+ * searching a particular item given it's key
  * 
+ * formal definition
+ * 
+ * a `bstree` is a `binary tree` that is either empty or satisfies 
+ * the following condtions:
+ * 1. all values occurring in the `left` subtree are smaller than that of 
+ * the root
+ * 2. all values occurring in the `right` are larger thatn that of the root
+ * 3. the `left` and `right` subtrees are themselves `bstrees`
+ * 
+ * a binary search tree inherits operations from binary tree
+ * 
+ * binary search tree constructors
+ * 1. `emptyTree()` - returns the `EmptyTree`
+ * 2. `makeTree(v, l, r)` - builds a `bstree` from a `root node` with 
+ * label `v` and two subtrees, `l` and `r`
+ * 
+ * binary search tree condition
+ * - `bstree.isEmpty()` - retuns false if `bstree` is non-empty
+ * 
+ * binary search tree selectors
+ * 1. `bstree.root()` - returns the value of `root node`
+ * 2. `bstree.left()` - returns the `left subtree` or `EmptyTree`
+ * 3. `bstree.right()` - returns the `right subtree` or `EmptyTree`
+ * 
+ * binary search tree mutator
+ * `insert(v, bstree)` - inserts a value `v` to the `bstree` as follows:
+ *
+ * 1. if `bstree.isEmpty() == true`, assign new value `v` to root node
+ * 2. if `bstree.isEmpty() == false`
+ * - if `v` < bstree.root()` then `insert(v, bstree.left())`
+ * - if `v` > bstree.root()` then `insert(v, bstree.right())`
+ * - if `v` == bstree.root()` then `ViolatedAssumptionError`
+ * 
+ * note that `insert(v, bstree)` assumes `v != bstree.root()`
  */
+
+export function getBST () {
+    let bstree = emptyTree();
+
+    if (arguments.length === 1) {
+        bstree = arguments[0];
+    }
+
+    return bstree;
+}
+
+// insert a value
+export function insert (v, bstree) {
+    if (bstree.isEmpty()) {
+        return getBST(makeBT(v));
+    } else if (v < bstree.root()) {
+        return getBST(makeBT(bstree.root(), insert(v, bstree.left()), bstree.right()));
+    } else if (v > bstree.root()) {
+        return getBST(makeBT(bstree.root(), bstree.left(), insert(v, bstree.right())));
+    } else {
+        throw {
+            name: 'ViolatedAssumptionError',
+            message: 'Can not replace root node'
+        }
+    }
+}
---
+++ chap7/bstree.test.js
@@ -0,0 +1,48 @@
+/**
+ * use nodejs repl session to interrogate the tree
+ * 
+ * while in repl, run `.load bstree.test.js`
+ * 
+ * operations
+ * 
+ * 1. `tree.isEmpty();`
+ * 2. `tree.root();`
+ * 3. `tree.left();`
+ * 4. `tree.right();`
+ */
+
+ async function bstree () {
+    let tree;
+    const bst = await import('./bstree.js');
+    tree = bst.getBST();
+    for (let n of nodes) {
+        tree = bst.insert(n, tree);
+    }
+    return tree;
+}
+
+let nodes = [8, 3, 1, 6, 7, 11, 14, 9, 10, 12, 15]; // insertion order
+
+let tree;
+
+bstree().then(value => {
+    tree = value;
+});
+
+let result = `
+           [8]
+           / \
+          /   \
+         /     \
+        /       \
+       /         \
+      /           \
+     [3]         [11]
+    /  \         /  \
+   /    \       /    \
+  /      \     /      \
+ [1]    [6]   [9]     [14]
+          \    \       / \
+           \    \     /   \
+           [7] [10] [12]  [15]
+`
