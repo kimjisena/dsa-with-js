4aea262b47ea86cb141880d6e9fcd80817a20295
jisena.btc <pjohmaths@gmail.com>
2022-06-21 17:46:23 +0300

    AVL single left and right rotations passing tests

--- chap7/avltree.test.js
+++ chap7/avltree.test.js
@@ -103,5 +103,40 @@ describe('avl-tree: core operations', () => {
         test('should return 9', () => {
             expect(avltree.right().root()).toBe(9);
         });
+
+        test('should return 5', () => {
+            expect(avltree.left().root()).toBe(5);
+        });
     });
+
+    describe('avl-tree: single right rotation', () => {
+        let avltree = getAVL(7,
+                            getAVL(5, 
+                                getAVL(3, getAVL(2), getAVL(1)),
+                                getAVL(6)),
+                            getAVL(9)
+                            );
+
+        test('should return 2', () => {
+            expect(avltree.height()).toBe(2);
+        });
+                    
+        test('should return 0', () => {
+            expect(avltree.getBalance()).toBe(0);
+        });
+
+        test('should return 5', () => {
+            expect(avltree.root()).toBe(5);
+        });
+
+        test('should return 7', () => {
+            expect(avltree.right().root()).toBe(7);
+        });
+
+        test('should return 3', () => {
+            expect(avltree.left().root()).toBe(3);
+        });
+    });
+
+    
 });
