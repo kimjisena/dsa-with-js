c5cf0acca205982f6a2cdf17a0da9655a5bd1034
jisena.btc <pjohmaths@gmail.com>
2022-06-27 20:50:33 +0300

    bubble sort passing tests

--- sort/bubble-sort/bubble-sort.js
+++ sort/bubble-sort/bubble-sort.js
@@ -1,14 +1,25 @@
 function bubbleSort (array) {
-    let temp;
+    let temp, top, flag, i;
 
-    for (let i = 1; i < array.length; i++) {
-        for (let j = n - 1; j >= i; j--) {
-            if (array[j] < array[j - 1]) {
-                temp = array[j];
-                array[j] = array[j - 1];
-                array[j - 1] = temp;
+    top = array.length;
+
+    do {
+        flag = false;
+        top--;
+        for (i = 0; i < top; i++) {
+            if (array[i] > array[i + 1]) {
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                flag = true;
             }
         }
-    }
+
+    } while(flag);
+
     return array;
 }
+
+module.exports = {
+    bubbleSort,
+};
